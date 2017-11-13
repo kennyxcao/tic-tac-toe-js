909c1de89cec6866513d54e4b75c01ae2325b5db
Kenny Cao <kennyxcao@gmail.com>
2017-11-13 15:42:47 -0800

    Some progress for tic-tac-toe game

--- index.js
+++ index.js
@@ -25,3 +25,96 @@
 
 // Try your best to work on this challenge without referring to outside resources. However, if you have to look things up online, go ahead. 
 
+function prompt(question, callback) {
+  var stdin = process.stdin;
+  var stdout = process.stdout;
+
+  stdin.resume();
+  stdout.write(question);
+
+  stdin.once('data', function (data) {
+    callback(data.toString().trim());
+  });
+}
+
+// prompt('Whats your name?', function (input) {
+//   console.log(input);
+//   process.exit();
+// });
+
+
+function displayBoard (board) {
+  for (var i = 0; i < board.length; i += 3) {
+    console.log(board[i], board[i + 1], board[i + 2]);
+  }
+}
+
+function playerInput () {
+  var marker;
+
+  while (marker !== 'X' || marker !== 'O') {
+    prompt('Player 1: Do you want to be X or O?', function(input) {
+      if (input !== 'X' || input !== '0') {
+        console.log('Please select a valid choice');
+        playerInput();
+      } else {
+
+      }
+    })
+  }
+}
+
+function placeMarker (board, marker, position) {
+  board[position] = marker;
+}
+
+function winCheck (board, marker) {
+  var winCombo = [ 
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6] ];
+
+  var markPos = [];
+
+  for (var i = 0; i < board.length; i++) {
+    if (board[i] === marker) {
+      markPos.push(i);
+    }
+  }
+
+}
+
+function chooseFirst () {
+
+}
+
+function spaceCheck (board, position) {
+
+}
+
+function fullBoardCheck (board) {
+
+}
+
+function playerChoice (board) {
+
+}
+
+function startNewGame () {
+  while (true) {
+    var theBoard = ['*', '*', '*', '*', '*', '*', '*', '*', '*'];
+    var gameOn = true;
+    var turn = chooseFirst();
+    console.log(turn, 'will go first');
+
+    while (gameOn) {
+      if (turn === 'Player 1') {
+        console.log('Player 1\'s Turn');
+        displayBoard(theBoard);
+      }
+    }
+  }
+}
+
+// var theBoard = ['*', '*', '*', '*', '*', '*', '*', '*', '*'];
+// displayBoard(theBoard);
